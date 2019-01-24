50a4da7ef5b7f7f6147be0f4a152fd0a15ede0a0
M-Mahnke <46992086+M-Mahnke@users.noreply.github.com>
2019-01-24 11:31:09 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+// https://medium.freecodecamp.org/how-to-make-a-beautiful-tiny-npm-package-and-publish-it-2881d4307f78
+module.exports = function tiny(string) {
+  if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
+  return string.replace(/\s/g, "");
+};
